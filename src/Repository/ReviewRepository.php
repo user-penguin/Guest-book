2d193e47e91d2a77bb25033471a56adeafc23618
user-penguin <kobzev96@gmail.com>
2019-10-28 22:58:19 +0700

    add new repo methods

--- src/Repository/ReviewRepository.php
+++ src/Repository/ReviewRepository.php
@@ -18,4 +18,61 @@ class ReviewRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Review::class);
     }
+
+    /**
+     * @param int $eventId
+     * @param int $size
+     * @param int $page
+     * @return array
+     */
+    public function getEventsForPagination(int $eventId, int $size, int $page)
+    {
+        $qb = $this->createQueryBuilder('r');
+
+        return $qb
+            ->join('r.user', 'user')
+            ->join('r.event', 'event')
+
+            ->select('r.id')
+            ->addSelect('r.text')
+            ->addSelect('r.rate')
+            ->addSelect('user.username')
+
+            ->where('event.id = :eventId')
+
+            ->setParameter('eventId', $eventId)
+            ->setFirstResult(($page - 1) * $size)
+            ->setMaxResults($size)
+
+            ->getQuery()
+            ->getArrayResult()
+            ;
+    }
+
+    /**
+     * @param int $eventId
+     * @return int|mixed
+     */
+    public function getCountOfRecords(int $eventId)
+    {
+        $qb = $this->createQueryBuilder('r');
+
+        $count = $qb
+            ->join('r.event', 'event')
+
+            ->select('count(r.id) as count')
+            ->addSelect('event.id')
+
+            ->where('event.id = :eventId')
+
+            ->groupBy('event.id')
+
+            ->setParameter('eventId', $eventId)
+
+            ->getQuery()
+            ->getArrayResult()
+        ;
+
+        return $count[0]['count'];
+    }
 }
